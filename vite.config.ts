89cf90d36ad6f3a0a76c69600df146084920557f
phong phan <phongphanq089@gmail.com>
2026-06-08 16:21:26 +0700

    chore : update build config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,11 @@ import tailwindcss from "@tailwindcss/vite"
 import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 const config = defineConfig({
-  plugins: [
-    tsconfigPaths({ projects: ["./tsconfig.json"] }),
-    devtools(),
-    tailwindcss(),
-
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: true, // enable link crawling for route tree generation
-      },
-      sitemap: {
-        enabled: true,
-        host: "http://localhost:3000",
-      },
-    }),
-    viteReact(),
-  ],
   resolve: {
     dedupe: ["react", "react-dom", "styled-components"],
   },
@@ -31,6 +15,32 @@ const config = defineConfig({
       overlay: false,
     },
   },
+  plugins: [
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tanstackStart({
+      srcDirectory: "src",
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+      },
+      pages: [
+        { path: "/" },
+        { path: "/project" },
+        { path: "/blog" },
+        { path: "/resources" },
+        { path: "/studio" },
+      ],
+      sitemap: {
+        enabled: true,
+        host: "http://localhost:3000",
+      },
+    }),
+
+    tailwindcss(),
+    viteReact(),
+    devtools(),
+    nitro(),
+  ],
 })
 
 export default config
